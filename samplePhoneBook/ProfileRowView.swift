8e5bf90fe2b1ba108da4fa595bac67a01d22d3e8
mi_iroha <chimosy21@gmail.com>
2021-03-10 22:24:54 +0900

    fix #3 コードレビューの指摘事項修正

--- samplePhoneBook/ProfileRowView.swift
+++ samplePhoneBook/ProfileRowView.swift
@@ -8,51 +8,44 @@
 import SwiftUI
 
 struct ProfileRowView: View {
-    @State var buttonText = "button"
+    var height = UIScreen.main.bounds.height
+    var width = UIScreen.main.bounds.width
     
     var body: some View {
         
-        ZStack {
-            Color.orange
-                .edgesIgnoringSafeArea(.all)
-            
-            HStack {
-                Image("koara")
-                    .resizable()
-                    .frame(width: 100, height: 120)
-                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                VStack {
-                    HStack {
-                        Text("コアラ 太郎")
-                            .font(.largeTitle)
-                        Spacer()
-                    }
-                    .padding()
-                    
-                    HStack {
+        HStack {
+            Image("koara")
+                .resizable()
+                .frame(width: 90, height: 100)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .padding(.leading)
+            VStack {
+                HStack {
+                    Text("コアラ 太郎")
+                        .font(.title)
                     Spacer()
-                        Button(action: {}) {
-                            Text("編集")
-                        }
-                        .foregroundColor(.red)
-                        Button(action: {}) {
-                            Text("削除")
-                        }
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    }
-                    .padding(.trailing)
                 }
+                .padding()
+                
+                HStack {
+                Spacer()
+                    Button(action: {}) {
+                        Text("編集")
+                    }
+                    .foregroundColor(.red)
+                    Button(action: {}) {
+                        Text("削除")
+                    }
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+                .padding(.trailing)
             }
-            .compositingGroup()
-            .shadow(radius: 10)
-            .background(Color.white) // 色も一緒に角を丸めたい
-            .overlay(
-                RoundedRectangle(cornerRadius: 23)
-                    .stroke(Color.gray, lineWidth: 1)
-                    
-            )
-            
         }
+        .background(Color.white)
+        .cornerRadius(20)
+        .padding()
+        .frame(width: width, height: height)
+        .shadow(radius: 10)
     }
 }
 
